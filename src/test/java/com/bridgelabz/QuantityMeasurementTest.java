a39f57f9762c6f0fdccab2f29ed6a06b05d502ad
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-13 15:51:01 +0530

    [Saurabh]Refactored: code and test to check equality of two Inch parameters

--- src/test/java/com/bridgelabz/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementTest.java
@@ -19,4 +19,22 @@ public class QuantityMeasurementTest {
         Feet feet1 = new Feet(0.0);
         Assert.assertEquals(feet,feet1);
     }
+    @Test
+    public void givingFeet_whenEquals_ShouldReturnNotEquals(){
+        Feet feet = new Feet(0.0);
+        Feet feet1 = new Feet(1.0);
+        Assert.assertNotEquals(feet,feet1);
+    }
+    @Test
+    public void giving0Inch_whenEquals_ShouldReturnEquals(){
+       Inch inch = new Inch(0.0);
+        Inch inch1 = new Inch(0.0);
+        Assert.assertEquals(inch,inch1);
+    }
+    @Test
+    public void giving0Inch_whenEquals_ShouldReturnNotEquals(){
+        Inch inch = new Inch(0.0);
+        Inch inch1 = new Inch(1.0);
+        Assert.assertNotEquals(inch,inch1);
+    }
 }
